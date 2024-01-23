3f3dc089c07eebf2f81b0531f2e7ce1390c85221
pieyx <ijujkh@gmail.com>
2024-01-23 11:21:49 +0300

    Initial Commit

---
+++ Shaby/ShabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShabyApp.swift
+//  Shaby
+//
+//  Created by Jood Khoja on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ShabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
